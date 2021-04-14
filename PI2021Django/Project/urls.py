9b11eaa91945770a3262414c78c384868217ef2b
João Vasconcelos <joaopvasconcelos@ua.pt>
2021-04-14 16:33:29 +0100

    hopefully fixed

---
+++ PI2021Django/Project/urls.py
@@ -0,0 +1,36 @@
+"""Project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from webapp import views
+
+urlpatterns = [
+
+
+    path('sensors/', views.sensors_overview, name="sensors_overview"),
+    path('sensors/list/', views.sensors_get, name="sensors_list"),
+    path('sensors/detail/<str:key>/', views.sensors_get_one, name="sensors_detail"),
+    path('sensors/create/', views.sensors_post, name="sensors_create"),
+    # path('sensors/update/<str:key>/', views.sensors_update, name="sensors_update"),
+    # path('sensors/delete/<str:key>/', views.sensors_delete, name="sensors_delete"),
+
+    # path('products/', views.overview, name="overview"),
+    # path('products/list/', views.get, name="list"),
+    # path('products/detail/<str:key>/', views.get_one, name="detail"),
+    # path('products/create/', views.post, name="create"),
+    # path('products/create_list/', views.post_list, name="create"),
+    # path('products/update/<str:key>/', views.update, name="update"),
+    # path('products/delete/<str:key>/', views.delete, name="delete"),
+]
---
+++ PI2021Django/webapp/models.py
@@ -0,0 +1,28 @@
+import uuid
+from cassandra.cqlengine import columns
+from cassandra.cqlengine.management import sync_table
+from django_cassandra_engine.models import DjangoCassandraModel
+
+
+class sensors(DjangoCassandraModel):
+    sensor_id = columns.Text(primary_key=True)
+    user = columns.Text()
+    tables = columns.List(value_type=columns.Text)
+    pks = columns.List(value_type=columns.Text)
+
+    class Meta:
+        unique_together = (('sensor_id', 'user'))
+
+# Examples
+# class products(DjangoCassandraModel):
+#     id = columns.UUID(primary_key=True)
+#     attributes = columns.Map(key_type=columns.Text, value_type=columns.Text)
+
+#     def __str__(self):
+#         return str(self.id)
+# 
+# class ExampleModel(DjangoCassandraModel):
+#     example_id    = columns.UUID(primary_key=True, default=uuid.uuid4)
+#     example_type  = columns.Integer(index=True)
+#     created_at    = columns.DateTime()
+#     description   = columns.Text(required=False)
