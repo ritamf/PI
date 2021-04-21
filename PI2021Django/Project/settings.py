81896639be7a336177891e67c8debf63527154d4
ritamf <ritaferrolho@ua.pt>
2021-04-21 16:37:16 +0100

    Set `replication_factor` to 1
    `replication_factor` needs to be equal to the number of nodes

--- PI2021Django/Project/settings.py
+++ PI2021Django/Project/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'OPTIONS': {
             'replication': {
                 'strategy_class': 'SimpleStrategy',
-                'replication_factor': 3
+                'replication_factor': 1
             },
             'connection': {
                 'consistency': ConsistencyLevel.ONE,
