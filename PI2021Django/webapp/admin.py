cd7eb358ae56d03942625e865b86d082bc3a7e93
ritamf <ritaferrolho@ua.pt>
2021-05-09 23:14:25 +0100

    Create basic admin page

--- PI2021Django/webapp/admin.py
+++ PI2021Django/webapp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import products, attributes
+from .models import sensors
 
 # Register your models here.
 
-admin.site.register(products)
-admin.site.register(attributes)
+admin.site.site_header = "DBoT Admin"
+admin.site.site_title = "DBoT Admin Area"
+admin.site.index_header = "Welcome to the DBoT admin area"
+
+admin.site.register(sensors)
