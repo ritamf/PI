e10a1373f19615b6ad6878fe787dc757b0acd14f
João Vasconcelos <joaopvasconcelos@ua.pt>
2021-06-12 16:15:59 +0100

    insert update

--- PI2021Django/webapp/views.py
+++ PI2021Django/webapp/views.py
@@ -137,7 +137,7 @@ def insert_into_db(request,user_token,sensorid):
     parsedJson = []
     for item in req:
         print(item)
-        jsonParserInit.flat_json(json.dumps(item))
+        jsonParserInit.flat_json(json.loads(item))
         parsedJson.append(item)
 
     h2 = hashlib.new('sha512_256')
